6d9f5115f2ef1f9cabde1cd59caf5f908d923f16
Stuart Sullivan <sullivanstuart94@gmail.com>
2017-05-19 04:55:18 -0400

    Solutions to Excercises

---
+++ 16_excercise-solu-p2/01/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func half(number int) (int, bool) {
+    return number/2, number%2 == 0
+}
+
+func main() {
+    fmt.Println(half(1))
+    fmt.Println(half(2))
+    fmt.Println(half(10))
+}
